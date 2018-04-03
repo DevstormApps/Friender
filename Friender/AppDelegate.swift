30355183b08fe7f1ec0d7c5d14587767f708ac7c
Snorlax <lenepouv@gmail.com>
2018-04-03 18:28:05 +0300

    First commit

--- Friender/AppDelegate.swift
+++ Friender/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyD0t_didG6IbHSJYJNdTDSOwaW5tvSPKS0")
+        GMSPlacesClient.provideAPIKey("AIzaSyD0t_didG6IbHSJYJNdTDSOwaW5tvSPKS0")
         return true
     }
 
--- Friender/MapVC.swift
+++ Friender/MapVC.swift
@@ -7,29 +7,89 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapVC: UIViewController {
+    
+    private let locationManager = CLLocationManager()
 
+
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    private func reverseGeocodeCoordinate(_ coordinate: CLLocationCoordinate2D) {
+        
+        // 1
+        let geocoder = GMSGeocoder()
+        
+        // 2
+        geocoder.reverseGeocodeCoordinate(coordinate) { response, error in
+            guard let address = response?.firstResult(), let lines = address.lines else {
+                return
+            }
+            
+            // 3
+            self.addressLabel.text = lines.joined(separator: "\n")
+            
+            let labelHeight = self.addressLabel.intrinsicContentSize.height
+            self.mapView.padding = UIEdgeInsets(top: self.view.safeAreaInsets.top, left: 0,
+                                                bottom: labelHeight, right: 0)
+            
+            // 4
+                  }
+    }
+    
+    @IBAction func onBtnWasPressed(_ sender: Any) {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - CLLocationManagerDelegate
+//1
+extension MapVC: CLLocationManagerDelegate {
+    // 2
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 3
+        guard status == .authorizedWhenInUse else {
+            return
+        }
+        // 4
+        locationManager.startUpdatingLocation()
+        
+        //5
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
     }
-    */
+    
+    // 6
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
+        // 7
+        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        
+        // 8
+        locationManager.stopUpdatingLocation()
+    }
+}
 
+// MARK: - GMSMapViewDelegate
+extension MapVC: GMSMapViewDelegate {
+    
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        reverseGeocodeCoordinate(position.target)
+    }
+    
 }
